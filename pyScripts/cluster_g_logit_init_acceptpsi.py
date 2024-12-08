46b411a1a6aa99bdce021fc8c3003d97b176212a
Sarah Urbut <sarahmurbut@gmail.com>
2024-12-08 11:06:31 -0500

    Update

--- pyScripts/cluster_g_logit_init_acceptpsi.py
+++ pyScripts/cluster_g_logit_init_acceptpsi.py
@@ -356,10 +356,10 @@ class AladynSurvivalFixedKernelsAvgLoss_clust_logitInit_psitest(nn.Module):
             print(f"Min:  {self.psi.grad.min().item():.4e}")
             
             # Check if psi is actually changing
-            old_psi = self.psi.detach().clone()
-            optimizer.step()
-            psi_change = (self.psi - old_psi).abs().mean().item()
-            print(f"Average psi change: {psi_change:.4e}")
+            ## old_psi = self.psi.detach().clone()
+            ## optimizer.step()
+            ## psi_change = (self.psi - old_psi).abs().mean().item()
+            ### print(f"Average psi change: {psi_change:.4e}")
 
 
             if epoch < 10 or epoch % 10 == 0:
